8c53d6780c67c159621d3869b5a728ce4a821427
RuiJy-X <rujzjmar@gmail.com>
2026-04-23 22:11:45 +0800

    Fixed: timezone set to asia/manila

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,7 @@
 <?php
+
+date_default_timezone_set('Asia/Manila');
+
     session_start();
     if (!isset($_SESSION['name'])) {
         header('Location: login.php');
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+date_default_timezone_set('Asia/Manila');
+
+
 session_start();
 if(isset($_POST["submit"])) {
     $_SESSION["name"] = $_POST["name"];
